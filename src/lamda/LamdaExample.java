0d027980d22915a7275f926f064111492471fba6
ganesan <ganesanofficials@gmail.com>
2023-06-03 16:19:46 +0530

    commit and push

--- src/lamda/LamdaExample.java
+++ src/lamda/LamdaExample.java
@@ -1,6 +1,7 @@
 package lamda;
 
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class LamdaExample {
@@ -14,6 +15,8 @@ public class LamdaExample {
         };
 
         Function<Integer,Integer> function1 = (a)-> a*a; // single line lambda expression
+
+        BiFunction<Integer,Integer,Integer> function2 = (a,b)->a+b+b; // single line lambda expression with BIFunction
 
 
     }
